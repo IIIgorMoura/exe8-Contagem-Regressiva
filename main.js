877f3b6e0b1cc5d04783d9544006c2e3c8c1f302
IIIgorMoura <igor2007moura@gmail.com>
2023-11-14 11:58:23 -0300

    a

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const tempoRestante = document.getElementById('tempoRestante');
 const dataRestante = document.getElementById('dataRestante');
+const preparando = document.getElementById('preparando')
 
 // data final
 const dataAlvo = new Date('2023-11-27 00:00:00').getTime();
 
-// função para atualizar a contagem regressiva e a barra de progresso
-function atualizarContagem() {
+// atualizar tempo
+const relogio = setInterval(() => {
     const agora = new Date().getTime();
 
     // diferenca em milissegundos
@@ -17,23 +18,24 @@ function atualizarContagem() {
     const minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
     const segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
 
+    console.log(`${dias} dias, ${horas} horas, ${minutos} minutos, ${segundos} segundos`);
+
     // inserir tempo restante
     dataRestante.textContent = `${dias} Dias`;
     tempoRestante.textContent = `${horas} Horas, ${minutos} Minutos, ${segundos} Segundos`;
 
     // update barra de progresso
-    const progresso = ((dataAlvo - agora) / (dataAlvo - new Date('2023-11-27 00:00:00').getTime())) * 100;
+    const tempoTotal = dataAlvo - new Date('2023-11-27 00:00:00').getTime();
+    const tempoDecorrido = tempoTotal - diferenca;
+    const progresso = (tempoDecorrido / tempoTotal) * 100;
     document.getElementById('progresso').style.width = progresso + '%';
 
     // parar contagem
     if (diferenca < 0) {
-        clearInterval(intervalo);
-        console.log('Fim');
+        clearInterval(relogio);
+        
+        dataRestante.textContent = `Acesse agora`
+        tempoRestante.textContent = `Aproveite todos os descontos`
+        preparando.textContent = `Tudo pronto`
     }
-}
-
-// chama a função inicialmente para iniciar a contagem
-atualizarContagem();
-
-// atualiza a cada segundo
-const intervalo = setInterval(atualizarContagem, 1000);
+}, 1000);
